04d2469137469d3118ea4892a052a966c1fcc8a7
thanasisskotis <146757769+thanasisskotis@users.noreply.github.com>
2025-08-06 12:44:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -53,6 +53,7 @@ app.get('/images/:boxId', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
